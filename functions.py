1dc9129487f5dfe6e8feec5b2ceb69cee7612357
Mikayla Konst <mikayla.konst@gmail.com>
2017-07-20 09:10:42 -0700

    Demonstrating how to define and call functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+def multiply(number1, number2):
+  product = number1 * number2
+  return product
+
+def average(number1, number2):
+  av = (number1 + number2) / 2
+  return av
+  
+a = int(input("Enter first number: ")
+b = int(input("Enter second number: ")
+
+product = multiply(a, b)
+print("The product of these numbers is " + str(product))
+
+average = average(a, b)
+print("The average of these numbers is " + str(average))
