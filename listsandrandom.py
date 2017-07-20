d2621c55b00500001a55a6d96cafa6053d52f099
Mikayla Konst <mikayla.konst@gmail.com>
2017-07-20 13:11:44 -0700

    Create listsandrandom.py

---
+++ listsandrandom.py
@@ -0,0 +1,33 @@
+# import all the things
+import random
+
+# define all the functions
+def doStuff():
+    print("stuff")
+
+# write all the code
+
+# Make a list
+#               0           1           2
+groceries = ["apples", 42, "carrots"]
+#               -3          -2          -1
+
+# Print a list
+print(groceries)
+
+# Retrieve an item from the list
+print(groceries[0])
+
+# Random numbers
+
+# generate a random integer between 1 and 5 (inclusive)
+myCoolNumber = random.randint(1,5)
+print(myCoolNumber)
+doStuff()
+
+
+# for loops
+# includes a, does NOT include b
+# for x in range(a,b):
+# myCoolList[a:b]
+# includes a, does NOT include b
