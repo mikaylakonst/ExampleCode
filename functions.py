c502f6f29ac84d283b0ce37c0df686895770d430
Mikayla Konst <mikayla.konst@gmail.com>
2017-07-20 09:11:24 -0700

    Forgot close parens - added them

--- functions.py
+++ functions.py
@@ -6,8 +6,8 @@ def average(number1, number2):
   av = (number1 + number2) / 2
   return av
   
-a = int(input("Enter first number: ")
-b = int(input("Enter second number: ")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
 product = multiply(a, b)
 print("The product of these numbers is " + str(product))
