5bb2f12aad51204a2b12e0b33c54fbd728a8eea1
Mikayla Konst <mikayla.konst@gmail.com>
2017-07-20 08:47:55 -0700

    Create loopsandvariables.py

---
+++ loopsandvariables.py
@@ -0,0 +1,13 @@
+# returns the total of all the numbers from 1 to n
+def numbers(n):
+  total = 0
+  for i in range(1, n + 1):
+    total = total + i
+  return total
+  
+# returns a string with all the numbers from 1 to n
+def numbersString(1, n + 1):
+  wholeString = ""
+  for i in range(1, n + 1):
+    wholeString = wholeString + str(i) + " "
+  return wholeString
