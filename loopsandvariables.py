0a0ce2482f895237f1ab3e7927269fbed0de8ee6
Mikayla Konst <mikayla.konst@gmail.com>
2017-07-20 08:50:43 -0700

    Add calls to functions

--- loopsandvariables.py
+++ loopsandvariables.py
@@ -1,3 +1,5 @@
+# Function definitions go at the top of your file
+
 # returns the total of all the numbers from 1 to n
 def numbers(n):
   total = 0
@@ -6,8 +8,16 @@ def numbers(n):
   return total
   
 # returns a string with all the numbers from 1 to n
-def numbersString(1, n + 1):
+def numbersString(n):
   wholeString = ""
   for i in range(1, n + 1):
     wholeString = wholeString + str(i) + " "
   return wholeString
+
+# Your main code goes down here
+
+user_input = int(input("Pick a number"))
+total = numbers(user_input)
+print(total)
+bigString = numbersString(user_input)
+print(bigString)
